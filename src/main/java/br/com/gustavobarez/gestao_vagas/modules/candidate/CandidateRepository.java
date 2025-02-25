483337122bfc1d1b963ee87e1a116d0a02733e61
barez <gbarez44@gmail.com>
2025-02-25 19:47:10 -0300

    feat: update candidate controller

---
+++ src/main/java/br/com/gustavobarez/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,11 @@
+package br.com.gustavobarez.gestao_vagas.modules.candidate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+    // pelo findBy o JPA entende que queremos fazer uma busca, e ele fara sozinho um select no banco de dados para verificar isso 
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);    
+}
