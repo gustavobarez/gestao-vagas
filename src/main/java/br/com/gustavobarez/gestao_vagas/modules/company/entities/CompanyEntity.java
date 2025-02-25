ebfd7551ff2d5116e04268e6b3f913a3b4d5e920
barez <gbarez44@gmail.com>
2025-02-25 19:45:24 -0300

    feat: create Company table on database

---
+++ src/main/java/br/com/gustavobarez/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,39 @@
+package br.com.gustavobarez.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity(name = "company")
+public class CompanyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Pattern(regexp = "\\S+", message = "O campo [username] nao deve conter espacos")
+    private String username;
+    @Email(message = "O campo [email] deve conter um e-mail valido")
+    private String email;
+    @Length(min = 3, max = 20, message = "A senha deve conter entre (3) e (20) caracteres")
+    private String password;
+    
+    private String website;
+    
+    private String name;
+    
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
