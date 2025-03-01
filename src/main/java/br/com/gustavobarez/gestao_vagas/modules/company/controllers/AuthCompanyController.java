7833f7049c67a9dc97fa9df7c70b81cb894f148d
barez <gbarez44@gmail.com>
2025-03-01 14:02:49 -0300

    feat: add Company authentication

---
+++ src/main/java/br/com/gustavobarez/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -0,0 +1,26 @@
+package br.com.gustavobarez.gestao_vagas.modules.company.controllers;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gustavobarez.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.gustavobarez.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCompanyController {
+    
+    @Autowired
+    private AuthCompanyUseCase authCompanyUseCase;
+
+    @PostMapping("/company")
+    public String create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+        return this.authCompanyUseCase.execute(authCompanyDTO);
+    }
+
+}
