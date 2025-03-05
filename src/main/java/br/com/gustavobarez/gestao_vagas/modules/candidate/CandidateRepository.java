e97b5389b8b88d68847877987cc8ff6aa2bef481
barez <gbarez44@gmail.com>
2025-03-05 20:26:52 -0300

    feat: add candidate auth and password cryptograph

--- src/main/java/br/com/gustavobarez/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/gustavobarez/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
     // pelo findBy o JPA entende que queremos fazer uma busca, e ele fara sozinho um select no banco de dados para verificar isso 
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);    
+    Optional<CandidateEntity> findByUsername(String username);
 }
